b2b14e94072540814d1c1ed4f9bc4c6d19abbb9b
Austin Bello <austin@automatedinsights.com>
2017-05-18 14:53:22 -0400

    homework final v2

--- homework3.js
+++ homework3.js
@@ -2,38 +2,38 @@ $(function() {
 
   var $form = $('.my-form-class');
 
-  var love_gif = ['http://www.reactiongifs.com/wp-content/uploads/2012/03/fez_I_love_you.gif',
-                  'http://mochimochiland.com/images/knittinggnome600.gif',
-                  'http://68.media.tumblr.com/7997e09918e89c3418f746f7c4eb72ef/tumblr_n3rmn9GDMB1s2wio8o1_500.gif'];
-  var maybe_gif = ['http://i.imgur.com/VYJUH2T.gif',
-                  'https://i.imgur.com/rvNEXjw.gif',
-                  'http://akns-images.eonline.com/eol_images/Entire_Site/20141011/rs_500x216-141111111701-tumblr_n32os5L7RY1sor9n5o1_500.gif'];
-  var nope_gif = ['http://www.reactiongifs.com/r/oplz.gif',
-                  'http://i.imgur.com/cqyUOW4.gif',
-                  'http://68.media.tumblr.com/b2852e8ca0fbadd752453f8223aabe93/tumblr_o37vpaN7qg1qmob6ro1_500.gif'];
-
-  var randomNumberBetween0and2 = Math.floor(Math.random() * 3);
-
-  var love = function(percentage) {
-    if (percentage >= 65) {
-      $('.target-image').prop('src', love_gif[randomNumberBetween0and2]);
-      $('.love_match').text("It's a Match!!!");
-      $('.maybe').text("");
-      $('.no_match').text("");
-    } else if (percentage >= 35) {
-      $('.target-image').prop('src', maybe_gif[randomNumberBetween0and2]);
-      $('.maybe').text("It is Undecided");
-      $('.no_match').text("");
-      $('.love_match').text("");
-    } else {
-      $('.target-image').prop('src', nope_gif[randomNumberBetween0and2]);
-      $('.no_match').text("This is Not a Match");
-      $('.love_match').text("");
-      $('.maybe').text("");
-    }
-  };
-
   $form.on('submit', function(event) {
+    var randomNumberBetween0and2 = Math.floor(Math.random() * 3);
+
+    var love_gif = ['http://www.reactiongifs.com/wp-content/uploads/2012/03/fez_I_love_you.gif',
+                    'http://mochimochiland.com/images/knittinggnome600.gif',
+                    'http://68.media.tumblr.com/7997e09918e89c3418f746f7c4eb72ef/tumblr_n3rmn9GDMB1s2wio8o1_500.gif'];
+    var maybe_gif = ['http://i.imgur.com/VYJUH2T.gif',
+                    'https://i.imgur.com/rvNEXjw.gif',
+                    'http://akns-images.eonline.com/eol_images/Entire_Site/20141011/rs_500x216-141111111701-tumblr_n32os5L7RY1sor9n5o1_500.gif'];
+    var nope_gif = ['http://www.reactiongifs.com/r/oplz.gif',
+                    'http://i.imgur.com/cqyUOW4.gif',
+                    'http://68.media.tumblr.com/b2852e8ca0fbadd752453f8223aabe93/tumblr_o37vpaN7qg1qmob6ro1_500.gif'];
+
+    var love = function(percentage) {
+      if (percentage >= 65) {
+        $('.target-image').prop('src', love_gif[randomNumberBetween0and2]);
+        $('.love_match').text("It's a Match!!!");
+        $('.maybe').text("");
+        $('.no_match').text("");
+      } else if (percentage >= 35) {
+        $('.target-image').prop('src', maybe_gif[randomNumberBetween0and2]);
+        $('.maybe').text("It is Undecided");
+        $('.no_match').text("");
+        $('.love_match').text("");
+      } else {
+        $('.target-image').prop('src', nope_gif[randomNumberBetween0and2]);
+        $('.no_match').text("This is Not a Match");
+        $('.love_match').text("");
+        $('.maybe').text("");
+      }
+    };
+
     event.preventDefault();
     console.log($(this).serializeArray());
 
